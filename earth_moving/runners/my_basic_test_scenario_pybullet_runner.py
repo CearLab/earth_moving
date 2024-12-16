d962cca249e4dfbe3041e7ad93fcf4a6da553649
Federico Oliva <federico.oliva.it@gmail.com>
2024-12-16 18:23:34 +0200

    dev: single step camera acquisition

--- earth_moving/runners/my_basic_test_scenario_pybullet_runner.py
+++ earth_moving/runners/my_basic_test_scenario_pybullet_runner.py
@@ -1,19 +1,34 @@
-from earth_moving.scenarios.basic_test_scenario import basic_test_scenario
+import sys
+PATH = '/home/fedeoli/Documents/Work/earth_moving/earth_moving/'
+sys.path.append(PATH)
+
+from scenarios.basic_test_scenario import basic_test_scenario
+
 pybullet_running_camera_config = {
     'backend_type': 'pybullet',    
     'sensor':
-        {'imgW': 180,
-         'imgH': 360,
-         'camera_pose_method': lambda t: 5*t + 2}, # TODO: still need to think about `t`
+        {
+         'imgW': 400,
+         'imgH': 400,
+         'camera_pose': [10.0, 10.0, 5.0],
+         'camera_up_vector': [0.0, 0.0, 1.0],
+         'target_pose': [0.0, 0.0, 0.0],
+         'fov': 30,
+         'near': 0.0,
+         'far': 10,
+         'save_path': 'earth_moving/outputs/fig/',
+         'camera_pose_method': lambda t: 5*t + 2 # TODO: still need to think about `t`
+        },
     'robot':
-        {'field1': 'blah',
-         'field2': 'blahblah',
+        {
+        },
     'simulation':
         {
-            'timedelta': 1.0/30,
+            'timedelta': 1.0/240,
+            'gravity': (0, 0, -9.81),
+            'gui': False
         }
     }
-}
 
 
 if __name__ == '__main__':
--- earth_moving/scenarios/basic_test_scenario.py
+++ earth_moving/scenarios/basic_test_scenario.py
@@ -1,4 +1,17 @@
-
+from ral.backend.base_backend import BaseBackend
 
 def basic_test_scenario(config):
-    pass
+    Basebackend = BaseBackend()
+    _simulation = config.get('simulation')
+    _backend = config.get('backend_type')
+    Backend = Basebackend.initiate_backend(simulation=_simulation, backend_type=_backend)
+    
+    _sensor = config.get('sensor')
+    _path = _sensor.get('save_path')
+    Camera = Backend.initiate_rgb_sensor(name='cam',sensor=_sensor)
+    
+    # while True:
+    Backend.step()
+    _data = Camera.get_data()
+    # Camera.plot_data(data=_data)
+    Camera.save_data(data=_data,path=_path, name='cam_image')
